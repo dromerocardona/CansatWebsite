a7213ca10a589c7a83a82098b43cc90e7f15feb1
Diego Romero-Cardona <dr0887@uah.edu>
2026-07-29 16:47:54 -0500

    Formatted codebase and added teams page

--- scripts.js
+++ scripts.js
@@ -1,15 +1,13 @@
 // Places the menu bar at the top of each page
-fetch('menubar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.body.insertAdjacentHTML('afterbegin', data);
-    });
-//
+fetch("menubar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.body.insertAdjacentHTML("afterbegin", data);
+  });
 
 // Places the footer at the bottom of each page
-fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.body.insertAdjacentHTML('beforeend', data);
-    });
-//
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.body.insertAdjacentHTML("beforeend", data);
+  });
